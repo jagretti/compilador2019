0c2345ae978f2a5edfa5c8ec5c7105c96902a2c4
lfigueroa <laurofigueroa@hotmail.com>
2019-11-02 20:40:12 -0300

    add parameterization to 'test.sh' to choose witch tiger's file(s) to test. Run --help for more information

--- entrega1/test.sh
+++ entrega1/test.sh
@@ -1,23 +1,50 @@
-#!/bin/sh
+#!/bin/bash
 
-TEST=../tests # complete with the path to the tests folder
+TEST_DIR=../tests # complete path to tests directory
 
-GOOD=${TEST}/good
-SYNTAX=${TEST}/syntax
-TYPE=${TEST}/type
+GOOD_DIR=${TEST_DIR}/good
+SYNTAX_DIR=${TEST_DIR}/syntax
+TYPE_DIR=${TEST_DIR}/type
 
-eval_test () {
+eval_test ()
+{
     for d in $1/* ; do
         if ./tiger $d | grep -q "bien!" ; then
             echo "$(tput setaf 2)[ok] $(tput sgr 0) $d"
         else
             echo "$(tput setaf 1)[err] $(tput sgr 0) $d"
 	    ./tiger $d
-        fi   
+        fi
     done
 }
 
-eval_test ${GOOD}
-eval_test ${SYNTAX}
-eval_test ${TYPE}
+usage()
+{
+    echo "[-g | --good]    to test tiger's files in ${GOOD_DIR}"
+    echo "[-s | --syntax]  to test tiger's files in ${SYNTAX_DIR}"
+    echo "[-t | --type]    to test tiger's files in ${TYPE_DIR}"
+}
 
+if [ $# -gt 0 ]; then
+    while [ "$1" != "" ]; do
+	case "$1" in        # match the argument with the options
+	    -g | --good)    eval_test ${GOOD_DIR}
+			    ;;
+	    -s | --syntax)  eval_test ${SYNTAX_DIR}
+			    ;;
+	    -t | --type)    eval_test ${TYPE_DIR}
+			    ;;
+	    -h | --help)    usage
+			    exit 1
+			    ;;
+	    * )             usage
+			    exit 1
+	esac
+	shift               # position in the next argument
+    done
+else
+    # no arguments run all test
+    eval_test ${GOOD_DIR}
+    eval_test ${SYNTAX_DIR}
+    eval_test ${TYPE_DIR}
+fi
